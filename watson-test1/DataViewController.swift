09a2e640d364d95774fe1d6ab1d4ce5e8ee6e9bd
Behnam Hajian <behnam@ca.ibm.com>
2016-06-27 11:39:10 -0400

    Initial Commit

---
+++ watson-test1/DataViewController.swift
@@ -0,0 +1,34 @@
+//
+//  DataViewController.swift
+//  watson-test1
+//
+//  Created by behnam hajian on 2016-06-27.
+//  Copyright © 2016 behnam hajian. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: String = ""
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        self.dataLabel!.text = dataObject
+    }
+
+
+}
+
